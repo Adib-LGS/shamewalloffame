d6000dc8f02bbe186b612ea87f9db1ba6cca8cc0
Adib-Legastelois <adib.legastelois@icloud.com>
2020-09-10 16:59:40 +0100

    Add show.pins page + Nav Links with Route name

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,21 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinsController extends AbstractController
 {
     /**
-     * @Route("/", name="pins.home")
+     * @Route("/", name="home")
      */
-    public function index(PinRepository $pinRepository)
+    public function index(PinRepository $pinRepository): Response
     {
         //dd($pinRepository->findAll());
 
         $pins = $pinRepository->findAll();
 
         return $this->render('pins/index.html.twig', compact('pins'));
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="pins.show")
+     */
+    public function show(Pin $pin): Response
+    {
+        //dd($pin);
+        return $this->render('pins/show.html.twig', compact('pin'));
     }
 }
