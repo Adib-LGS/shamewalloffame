0d8788b0885bf9123542f6fb4dd993f78db0f24c
Adib-Legastelois <adib.legastelois@icloud.com>
2020-09-15 15:59:55 +0100

    Using @Assert Validation in Pin Entity

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -6,6 +6,7 @@ use App\Entity\Traits\Timestampable;
 use App\Repository\PinRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
@@ -26,11 +27,15 @@ class Pin
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Length(min=3)
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     * @Assert\Length(min=10)
      */
     private $description;
 
