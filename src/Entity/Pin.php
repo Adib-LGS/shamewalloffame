fe7acfd6d4f63270417a7390ddb1e8631aa39636
Adib-Legastelois <adib.legastelois@icloud.com>
2020-09-16 14:49:19 +0100

    Add image_name to field in pins table

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -39,6 +39,11 @@ class Pin
      */
     private $description;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageName;
+
     
 
     public function getId(): ?int
@@ -66,6 +71,18 @@ class Pin
     public function setDescription(?string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(?string $imageName): self
+    {
+        $this->imageName = $imageName;
 
         return $this;
     }
