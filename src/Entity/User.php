417222f8748eb3c5a91cba7253e7de2ae48f8ad2
Adib-Legastelois <adib.legastelois@icloud.com>
2020-09-17 17:20:14 +0100

    Display pin owner + add getFullName method in User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -196,4 +196,9 @@ class User implements UserInterface
         return $this;
     }
 
+    public function getFullName(): string
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName();
+    }
+
 }
